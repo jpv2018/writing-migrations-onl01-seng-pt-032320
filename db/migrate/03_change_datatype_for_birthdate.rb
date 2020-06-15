31b62bb9bfbbf945882bd5d68771d20a459323f1
Jason Viola <jason.viola@mavs.uta.edu>
2020-06-15 19:51:44 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,6 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
-end
 
-def change
-  change_column(:students, :birthdate, datetime)
+  def change
+    change_column(:students, :birthdate, datetime)
+  end
 end
